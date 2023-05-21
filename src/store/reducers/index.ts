2cd9c05f148521d2fd0293d2e2f7f3ab9419431c
Andile Msani <andile@ikhokha.com>
2023-05-21 13:04:56 +0200

    add users reducer to root

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 
 import authReducer from "./auth-reducer";
+import usersReducer from "./users-reducer";
 
 
 
 const rootReducer: any = combineReducers({
   authReducer,
+  usersReducer,
 });
 
 export default rootReducer;
